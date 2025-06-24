c9cd51087267729b74ffa1e1ce4de2d4a6f8d7b5
Valter Sales <vsalesjr2008@gmail.com>
2025-06-24 10:54:28 -0300

    Algumas melhoras

--- Conversor/script.js
+++ Conversor/script.js
@@ -1,18 +1,18 @@
-let bit = 350000
-let euro = 6.37
-let dolar = 5.5
-const real = parseFloat(document.getElementById("real").value)
+const bit = 350000
+const euro = 6.37
+const dolar = 5.5
 
 function calcular_conversor(a,b){
     return a/b
 }
 
 function mostrar(){
+    
     const real = parseFloat(document.getElementById("real").value)
     let resultD = calcular_conversor(real,dolar)
     let resultE = calcular_conversor(real,euro)
     let resultB = calcular_conversor(real,bit)
-    document.getElementById("dolar").textContent = "Dolar: " + resultD
-    document.getElementById("euro").textContent = "Euro: " + resultE
-    document.getElementById("bitcoin").textContent = "Bitcoin: " + resultB
+    document.getElementById("dolar").textContent = "Dolar: " + resultD.toFixed(2)
+    document.getElementById("euro").textContent = "Euro: " + resultE.toFixed(2)
+    document.getElementById("bitcoin").textContent = "Bitcoin: " + resultB.toFixed(8)
 }
